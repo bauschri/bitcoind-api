ccbcf8d745a3085fcbf080a1c6c71320c3452407
Christian Baus <chris@cbaus.de>
2019-01-01 13:39:34 +0100

    fix

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const kue = require('kue');
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const port = 4001
+const config = require('./config');
+const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
-const server = kue.app.listen(3000, () => {
-	const host = server.address().address;
-  const port = server.address().port;
-	console.log('Starting server listening at http://%s:%s ', host, port);
+
+const db = require('./models').initializeDB(config);
+
+require('./worker/index')({io})
+require('./models/resources')({ app, config, db });
+
+app.get('/test',function(req,res) {
+  io.sockets.emit('change color','triggered');
+  res.status(200).json({})
 });
+
+io.on('connection', socket => {
+  console.log('User connected', socket.id)
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
